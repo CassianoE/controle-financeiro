17aeec614fb71f12913048a8031f20739ca889c3
CassianoE <kssycs@outlook.com>
2025-05-22 16:30:33 -0300

    category crud

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Category;
+use Illuminate\Support\Collection;
+
+class CategoryRepository
+{
+
+    public function getAllByUserId(int $userId): Collection
+    {
+        return Category::where('user_id', $userId)->get();
+    }
+
+    public function find(int $id): Category
+    {
+        return Category::findOrFail($id);
+    }
+
+    public function create(array $data): Category
+    {
+        return Category::create($data);
+    }
+
+    public function update(Category $category, array $data): Category
+    {
+        $category->update($data);
+        return $category;
+    }
+
+    public function delete(Category $category): void
+    {
+        $category->delete();
+    }
+
+
+}
---
+++ app/Services/CategoryService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+use App\Repositories\CategoryRepository;
+use Illuminate\Support\Collection;
+
+class CategoryService
+{
+
+    public function __construct(
+        protected CategoryRepository $categoryRepository
+    ) {}
+
+    public function list (int $userId): Collection
+    {
+        return $this->categoryRepository->getAllByUserId($userId);
+    }
+
+    public function store (array $data, int $userId): Category
+    {
+        $data['user_id'] = $userId;
+        return $this->categoryRepository->create($data);
+    }
+
+    public function update(Category $category, array $data): Category
+    {
+        return $this->categoryRepository->update($category, $data);
+    }
+
+    public function delete(Category $category): void
+    {
+        $this->categoryRepository->delete($category);
+    }
+
+
+    
+}
+
