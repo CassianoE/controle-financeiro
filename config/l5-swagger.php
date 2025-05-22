863a2d04ef41d7bc391b2c3fe99cc4988f60edeb
CassianoE <kssycs@outlook.com>
2025-05-22 18:28:24 -0300

    updating swagger docs

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -65,6 +65,7 @@ return [
              * Middleware allows to prevent unexpected access to API documentation
              */
             'middleware' => [
+                'auth:sanctum',
                 'api' => [],
                 'asset' => [],
                 'docs' => [],
