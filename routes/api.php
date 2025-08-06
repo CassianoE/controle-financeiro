2395a1cbac4cd3ddd4a699627630ac30185aaf37
CassianoE <kssycs@outlook.com>
2025-08-05 22:15:37 -0300

    Add budgets resource and clean up middleware

--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,7 @@ use App\Http\Controllers\Api\TransactionController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Api\AccountController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Api\BudgetController;
 
 Route::prefix('auth')->group(function () {
     Route::post('register', [RegisterController::class, 'register']);
@@ -25,14 +15,19 @@ Route::prefix('auth')->group(function () {
     Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 });
 
-// Rotas personalizadas de transactions antes do resource
-Route::prefix('transactions')->middleware('auth:sanctum')->group(function () {
-    Route::get('balance', [TransactionController::class, 'getBalance']);
-    Route::get('period', [TransactionController::class, 'getByPeriod']);
-    Route::get('summary', [TransactionController::class, 'getSummaryByPeriod']);
-});
+// <--- GRUPO DE ROTAS PROTEGIDAS POR AUTH:SANCTUM
+Route::middleware('auth:sanctum')->group(function () {
 
-// Rotas de recurso
-Route::apiResource('categories', CategoryController::class)->middleware('auth:sanctum');
-Route::apiResource('transactions', TransactionController::class)->middleware('auth:sanctum')->except(['show']);
-Route::apiResource('account', AccountController::class)->middleware('auth:sanctum');
+    // Rotas personalizadas de transactions
+    Route::prefix('transactions')->group(function () {
+        Route::get('balance', [TransactionController::class, 'getBalance']);
+        Route::get('period', [TransactionController::class, 'getByPeriod']);
+        Route::get('summary', [TransactionController::class, 'getSummaryByPeriod']);
+    });
+
+    // Rotas de recurso
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('transactions', TransactionController::class)->except(['show']);
+    Route::apiResource('accounts', AccountController::class);
+    Route::apiResource('budgets', BudgetController::class);
+});
