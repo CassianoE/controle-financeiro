9fbbfc58bffaab3bc6a465f026ce98e7eddbc76e
CassianoE <kssycs@outlook.com>
2025-11-15 17:51:47 -0300

    adicionando bindings para BudgetRepository e TransactionRepository no AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,14 @@
 namespace App\Providers;
 
 use App\Repositories\UserRepository;
-use App\Repositories\TransactionRepository;
-use Illuminate\Support\ServiceProvider;
-use App\Repositories\Contracts\UserRepositoryInterface;
-use App\Repositories\Contracts\TransactionRepositoryInterface;
-use App\Repositories\Contracts\AccountRepositoryInterface;
+use App\Repositories\BudgetRepository;
 use App\Repositories\AccountRepository;
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\TransactionRepository;
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\Contracts\BudgetRepositoryInterface;
+use App\Repositories\Contracts\AccountRepositoryInterface;
+use App\Repositories\Contracts\TransactionRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,6 +31,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             AccountRepositoryInterface::class,
              AccountRepository::class
+        );
+
+        $this->app->bind(
+            BudgetRepositoryInterface::class,
+             BudgetRepository::class
         );
     }
 
